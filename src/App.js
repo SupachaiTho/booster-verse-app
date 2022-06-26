7e07ae4eaa83c95ed1c075e1bb732892a04ef4eb
Joe <joe@fazwaz.com>
2022-06-26 22:00:18 +0700

    init app

--- src/App.js
+++ src/App.js
@@ -39,14 +39,28 @@ function App() {
       [devicePixelRatio]
   );
 
-
-
   return (
-    <Unity
-        unityProvider={unityProvider}
-        style={{ width: 1600, height: 900 }}
-        devicePixelRatio={devicePixelRatio}
-    />
+      <div className="App">
+          <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <p>
+                  Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                  className="App-link"
+                  href="https://reactjs.org"
+                  target="_blank"
+                  rel="noopener noreferrer"
+              >
+                  Learn React
+              </a>
+          </header>
+          <Unity
+              unityProvider={unityProvider}
+              style={{ width: 1600, height: 900 }}
+              devicePixelRatio={devicePixelRatio}
+          />
+      </div>
   );
 }
 
